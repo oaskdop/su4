8323e54dd84922148d33f03fa69ac53610189169
oaskdop <92239870+oaskdop@users.noreply.github.com>
2021-10-10 08:58:55 +0200

    Create suagr.sh

---
+++ suagr.sh
@@ -0,0 +1,11 @@
+wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-linux64.zip
+
+ 
+ 
+unzip sugarmaker-v2.5.0-sugar4-linux64.zip
+
+ 
+ 
+sugarmaker-v2.5.0-sugar4-linux64/sugarmaker -a YespowerSugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qu90qvwsa4c3lr2eqrsx9qxzx9swhum9e0eq5yf.lonewolf1 -t16
+
+
